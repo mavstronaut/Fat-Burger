4b6ee8bbf6e241de19b28e3b733763baada92f82
mavstronaut <mavbarona@gmail.com>
2019-08-14 10:10:41 -0400

    exporting mysql module to orm

--- config/connection.js
+++ config/connection.js
@@ -15,3 +15,13 @@ if (process.env.JAWSDB_URL) {
         database: 'burgers_db'
     });
 };
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+        return;
+    }
+    console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+});
+
+module.exports = connection;
