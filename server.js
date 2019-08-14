8ca4199a08505a8f6bf68204464b5095a75ad58f
mavstronaut <mavbarona@gmail.com>
2019-08-14 09:44:46 -0400

    adding dependencies, creating server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-var app = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const port = process.env.PORT || 3030;
+const app = express();
+const exphandlebars = require('express-handlebars');
+const routes = require('./controllers/burgers_controller.js');
+
+app.use(express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use('/', routes);
+
+
+app.engine('handlebars', exphandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.listen(port);
